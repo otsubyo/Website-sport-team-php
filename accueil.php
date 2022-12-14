36dab3e0cb04ed7316ebdd1e5fb52b00907b11a4
Anrifou Amdjad <amdjad.anrifou@etu.iut-tlse3.fr>
2022-12-14 17:14:46 +0100

    Modification

--- accueil.php
+++ accueil.php
@@ -2,3 +2,116 @@
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Se connecter</title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+        <link rel="shortcut icon" type="image/jpg" href="data/basketball-hoop.png" />
+    </head>
+    <body>
+        <!-- Menu de navigation -->
+        <div class="barre_nav">
+            <a href="#home">Accueil</a>
+            <div class="subnav">
+                <button class="subnavbtn">Gestion <i class="fa fa-caret-down"></i></button>
+                <div class="subnav-content">
+                <a href="#gest_match">Gestion des matchs</a>
+                <a href="#gest_joueur">Gestion des joueurs</a>
+                </div>
+            </div>
+            <a href="#home">Feuille de match</a>
+            <a href="#home">Statistique</a>
+            <a href="#exit">Deconnexion</a>
+        </div>
+        <div class="match_view">
+
+        </div>
+        
+           
+    </body>
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap');
+        body {
+            margin: 0;
+            padding: 0;
+            background: #2b3d58;
+            height: 100vh;
+            font-family: 'Noto Sans TC', sans-serif;
+        }
+
+        /* The navigation menu */
+        .barre_nav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        /* Navigation links */
+        .barre_nav a {
+            float: left;
+            font-size: 16px;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* The subnavigation menu */
+        .subnav {
+            float: left;
+            overflow: hidden;
+        }
+
+        /* Subnav button */
+        .subnav .subnavbtn {
+            font-size: 16px;
+            border: none;
+            outline: none;
+            color: white;
+            padding: 14px 16px;
+            background-color: inherit;
+            font-family: inherit;
+            margin: 0;
+        }
+
+        /* Add a red background color to navigation links on hover */
+        .barre_nav a:hover, .subnav:hover .subnavbtn {
+            background-color: #111;
+        }
+
+        /* Style the subnav content - positioned absolute */
+        .subnav-content {
+            display: none;
+            position: absolute;
+            left: 0;
+            background-color: #111;
+            width: 100%;
+            z-index: 1;
+        }
+
+        /* Style the subnav links */
+        .subnav-content a {
+            float: left;
+            color: white;
+            text-decoration: none;
+        }
+
+        /* Add a grey background color on hover */
+        .subnav-content a:hover {
+            background-color: #eee;
+            color: black;
+        }
+
+        /* When you move the mouse over the subnav container, open the subnav content */
+        .subnav:hover .subnav-content {
+            display: block;
+        }
+
+        .match_view{
+            background-color: #fff;
+            width: 100%;
+            height: 80%;
+        }
+    </style>
--- connexion.php
+++ connexion.php
@@ -4,11 +4,12 @@
     (string) $username = NULL;
     (string) $userpwd = NULL;
     (array) $data = NULL;
+    (string) $color_mdp = '#2b3d58';
     
     if (isset($_POST['btn_connexion'])){
         // Utilisateur connecté
         $server = "localhost";
-        $db = "contact_db";
+        $db = "sport-team-management";
         $login = "root";
         $mdp = "9dfe351b";
         try {
@@ -20,147 +21,148 @@
 
         $ch_username = hash("sha256",$_POST['user_id']);
         $ch_pwd = hash("sha256",$_POST['user_pwd']);
-        $req = $linkpdo->prepare('SELECT * FROM user_connect');
-        $req->execute();
-        $data = $req->fetch();
-        $username = $data['user_name'];
-        $data = $req->fetch();
-        $userpwd = $data['pass_pwd'];
-        $req->closeCursor();
+        $res = $linkpdo->query('SELECT * FROM user_connect');
 
-        if (strcmp($ch_username,$username) == 0 && strcmp($ch_pwd,$userpwd)) {
-            echo "User connected";
-        } else {
-            echo "User not connected";
+        // Affichage des entrées du résultat une à une
+        while ($data = $res->fetch()) {
+            $userpwd = $data['pass_wd'];
+            $username = $data['user_name'];
+        }
+        $res->closeCursor();
+        
+        if (!(strcmp($ch_username,$username) == 0 && strcmp($ch_pwd,$userpwd) == 0)) {
+            $color_mdp = '#d00412';
+        } else{
+            $color_mdp = '#2b3d58';
+            header('location: accueil.php');
         }
     }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Se connecter</title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+        <link rel="shortcut icon" type="image/jpg" href="data/basketball-hoop.png" />
+    </head>
 
-<head>
-    <meta charset="UTF-8">
-    <title>Se connecter</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
-    <link rel="shortcut icon" type="image/jpg" href="data/basketball-hoop.png" />
-</head>
-
-<body>
-    <div class="center">
-        <div class="header">
-            Connexion
+    <body>
+        <div class="center">
+            <div class="header">
+                Connexion
+            </div>
+            <form action="" method="post">
+                <input type="text" placeholder="Nom d'utilisateur" name="user_id">
+                <i class="far fa-envelope"></i>
+                <input id="pswrd" type="password" placeholder="Mot de passe" name="user_pwd">
+                <i class="fas fa-lock" onclick="show()"></i>
+                <input type="submit" value="Connexion" name="btn_connexion" style="background-color: <?=$color_mdp?>">
+            </form>
         </div>
-        <form action="#" method="post">
-            <input type="text" placeholder="Nom d'utilisateur" name="user_id">
-            <i class="far fa-envelope"></i>
-            <input id="pswrd" type="password" placeholder="Mot de passe" name="user_pwd">
-            <i class="fas fa-lock" onclick="show()"></i>
-            <input type="submit" value="Connexion" name="btn_connexion">
-            <a href="#">Mot de passe oublié ?</a>
-        </form>
-    </div>
-</body>
-<script>
-    function show() {
-        var pswrd = document.getElementById('pswrd');
-        var icon = document.querySelector('.fas');
-        if (pswrd.type === "password") {
-            pswrd.type = "text";
-            pswrd.style.marginTop = "20px";
-            icon.style.color = "#ed7e0e";
-        } else {
-            pswrd.type = "password";
-            icon.style.color = "grey";
+    </body>
+    <script>
+        function show() {
+            var pswrd = document.getElementById('pswrd');
+            var icon = document.querySelector('.fas');
+            if (pswrd.type === "password") {
+                pswrd.type = "text";
+                pswrd.style.marginTop = "20px";
+                icon.style.color = "#ed7e0e";
+            } else {
+                pswrd.type = "password";
+                icon.style.color = "grey";
+            }
         }
-    }
-</script>
-<style>
-    @import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap');
+    </script>
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap');
 
-    body {
-        margin: 0;
-        padding: 0;
-        background: #2b3d58;
-        height: 100vh;
-        overflow: hidden;
-        font-family: 'Noto Sans TC', sans-serif;
-    }
+        body {
+            margin: 0;
+            padding: 0;
+            background: #2b3d58;
+            height: 100vh;
+            overflow: hidden;
+            font-family: 'Noto Sans TC', sans-serif;
+        }
 
-    .center {
-        width: 430px;
-        margin: 130px auto;
-        position: relative;
-    }
+        .center {
+            width: 430px;
+            margin: 130px auto;
+            position: relative;
+        }
 
-    .center .header {
-        font-size: 28px;
-        font-weight: bold;
-        color: white;
-        padding: 25px 0 30px 25px;
-        background: #7c8594;
-        border-bottom: 1px solid #7c8594;
-        border-radius: 5px 5px 0 0;
-    }
+        .center .header {
+            font-size: 28px;
+            font-weight: bold;
+            color: white;
+            padding: 25px 0 30px 25px;
+            background: #7c8594;
+            border-bottom: 1px solid #7c8594;
+            border-radius: 5px 5px 0 0;
+        }
 
-    .center form {
-        position: absolute;
-        background: white;
-        width: 100%;
-        padding: 50px 10px 0 30px;
-        box-sizing: border-box;
-        border: 1px solid #eeeee4;
-        border-radius: 0 0 5px 5px;
-    }
+        .center form {
+            position: absolute;
+            background: white;
+            width: 100%;
+            padding: 50px 10px 0 30px;
+            box-sizing: border-box;
+            border: 1px solid #eeeee4;
+            border-radius: 0 0 5px 5px;
+        }
 
-    form input {
-        height: 50px;
-        width: 90%;
-        padding: 0 10px;
-        border-radius: 3px;
-        border: 1px solid silver;
-        font-size: 18px;
-        outline: none;
-    }
+        form input {
+            height: 50px;
+            width: 90%;
+            padding: 0 10px;
+            border-radius: 3px;
+            border: 1px solid silver;
+            font-size: 18px;
+            outline: none;
+        }
 
-    form input[type="password"] {
-        margin-top: 20px;
-    }
+        form input[type="password"] {
+            margin-top: 20px;
+        }
 
-    form i {
-        position: absolute;
-        font-size: 25px;
-        color: grey;
-        margin: 15px 0 0 -45px;
-    }
+        form i {
+            position: absolute;
+            font-size: 25px;
+            color: grey;
+            margin: 15px 0 0 -45px;
+        }
 
-    i.fa-lock {
-        margin-top: 35px;
-    }
+        i.fa-lock {
+            margin-top: 35px;
+        }
 
-    form input[type="submit"] {
-        margin-top: 40px;
-        margin-bottom: 40px;
-        width: 130px;
-        height: 45px;
-        color: white;
-        cursor: pointer;
-        line-height: 45px;
-        border-radius: 45px;
-        border-radius: 5px;
-        background: #2b3d58;
-    }
+        form input[type="submit"] {
+            margin-top: 40px;
+            margin-bottom: 40px;
+            width: 130px;
+            height: 45px;
+            color: white;
+            cursor: pointer;
+            line-height: 45px;
+            border-radius: 45px;
+            border-radius: 5px;
+            margin-left: 30%;
+            margin-right: 30%;
+        }
 
-    form input[type="submit"]:hover {
-        background: #ed7e0e;
-        transition: .5s;
-    }
+        form input[type="submit"]:hover {
+            background: #0e1d35;
+            transition: .5s;
+        }
 
-    form a {
-        text-decoration: none;
-        font-size: 18px;
-        color: #2b3d58;
-        padding: 0 0 0 20px;
-    }
-</style>
+        form a {
+            text-decoration: none;
+            font-size: 18px;
+            color: #2b3d58;
+            padding: 0 0 0 20px;
+        }
+    </style>
 </html>
