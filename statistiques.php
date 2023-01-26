2c096f3a13e34292ace82558043ff0bab863370f
otsubyo <carl.premi@etu.iut-tlse3.fr>
2023-01-26 17:57:49 +0100

    test2 stats

--- statistiques.php
+++ statistiques.php
@@ -9,8 +9,24 @@ $mdp = "9dfe351b";
 
 // Vérification de la connexion
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Échec de la connexion: " . $conn->connect_error);
 } else {
-    echo "Connected successfully";
+    echo "Connexion réussie";
 }
 
+//Requête SQL pour compter le nombre total de matchs
+$sql = "SELECT COUNT(*) FROM matchs";
+$result = $conn->query($sql);
+$total_matches = $result->fetch_assoc();
+
+//Requête SQL pour récupérer les informations sur un joueur :
+$sql = "SELECT statut, poste, selections, evaluations, win_percent FROM joueurs WHERE id = '$player_id'";
+$result = $conn->query($sql);
+$player_data = $result->f
+
+
+
+
+
+
+?>
