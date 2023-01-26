b9313977c9773d4047a3272ca32450b055d398ea
otsubyo <carl.premi@etu.iut-tlse3.fr>
2023-01-26 18:24:26 +0100

    validation autre stats

--- statistiques.php
+++ statistiques.php
@@ -19,23 +19,38 @@ $sql = "SELECT COUNT(*) FROM matchs";
 $result = $conn->query($sql);
 $total_matches = $result->fetch_assoc();
 
-/*//Requête SQL pour récupérer les informations sur un joueur :
-$sql = "SELECT nom, prenom, taille, poids, poste, statut, statut, poste, selections, evaluations, win_percent FROM joueurs WHERE id = '$numero_licence'";
-$result = $conn->query($sql);
-$player_data = $result->fetch_assoc();
-*/
+// Calcul des pourcentages
+$total = $row['total'];
+$won = $row['won'];
+$lost = $row['lost'];
+$wonPercent = round($won / $total * 100, 2);
+$lostPercent = round($lost / $total * 100, 2);
+
+// Affichage des résultats
+echo "<h2>Statistiques des matchs</h2>";
+echo "<p>Nombre total de matchs : $total</p>";
+echo "<p>Nombre de matchs gagnés : $won ($wonPercent%)</p>";
+echo "<p>Nombre de matchs perdus : $lost ($lostPercent%)</p>";
 
 
 
-$numero_licence = $_POST['numero_licence'];
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $taille = $_POST['taille'];
-    $poids = $_POST['poids'];
-    $poste = $_POST['poste'];
-    $statut = $_POST['statut'];
 
+
+
+
+// Fermeture de la connexion à la base de données
+$conn->close();
 ?>
+
+
+
+
+
+
+
+
+
+
 
 
 <!DOCTYPE html>
